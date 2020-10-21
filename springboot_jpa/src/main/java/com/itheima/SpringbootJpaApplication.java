49dc099807f0088bdbbfc063fe7c8e6d06a7c2a0
zhaodw <2462215838@qq.com>
2020-10-21 17:57:40 +0800

    测试密码

--- springboot_jpa/src/main/java/com/itheima/SpringbootJpaApplication.java
+++ springboot_jpa/src/main/java/com/itheima/SpringbootJpaApplication.java
@@ -2,7 +2,7 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class SpringbootJpaApplication {
 
