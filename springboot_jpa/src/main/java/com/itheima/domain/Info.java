ce73c484766eca2201b28dc78241b60b6fe7591c
zhaodw <2462215838@qq.com>
2021-04-09 06:47:20 +0800

    IDEA提交测试两个仓库

---
+++ springboot_jpa/src/main/java/com/itheima/domain/Info.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+import lombok.Data;
+
+/**
+ * Created by zhaodw on 2021/4/9 6:44
+ */
+@Data
+public class Info {
+    private String address;
+    private String number;
+}
