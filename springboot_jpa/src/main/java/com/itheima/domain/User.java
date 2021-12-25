e840d93407ab66693a6994d0965df34b596d0577
zhaodw <54707352+zdw-1011@users.noreply.github.com>
2021-12-25 08:42:33 +0000

    Update User.java

--- springboot_jpa/src/main/java/com/itheima/domain/User.java
+++ springboot_jpa/src/main/java/com/itheima/domain/User.java
@@ -12,7 +12,7 @@ public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
+    private String usernames;
     private String password;
     private String name;
     public Long getId() {
